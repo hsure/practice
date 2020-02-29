c48ff27dbb5ab8de5ac7b5b00a8677f44ecb570b
prasanna <prasannapadmalatha@gmail.com>
2020-03-01 03:15:09 +0530

    sample

---
+++ Char.java
@@ -0,0 +1,24 @@
+class Char
+{
+	public static void main(String[] args) 
+	{
+		char i,j;
+	
+	
+	for(i='A';i<='E';i++)
+		{
+		
+		for(j='A';j<='E';j++)
+			{
+			System.out.print(j);
+			}
+			System.out.println();
+		
+			}
+	}
+	}
+	
+
+
+
+
